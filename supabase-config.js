93d9a7b9419e499be98053661e2d2d9cf37d6430
jotaveeara <joaovitoraraujo120622@gmail.com>
2026-08-12 23:32:38 -0400

    login minha-dieta beta 3

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://xygywrybxdukglpnpoxc.supabase.co",
+  url: "https://xgygwrybxdukglpnpoxc.supabase.co",
   publishableKey: "sb_publishable_MYCvSZ5LEs0qe310NycBuw_mM97-pvu"
 };
